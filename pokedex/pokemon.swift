d35748bff33acfc2dce4e57ca2a70eaad1246385
makenaw <makena.walsh@gmail.com>
2016-01-26 13:33:35 +0800

    creates pokemon model

---
+++ pokedex/pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  pokemon.swift
+//  pokedex
+//
+//  Created by makena  on 1/26/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
