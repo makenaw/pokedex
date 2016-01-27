f8f01f0bb63398b3811f2eadf1a699d7f25ccedb
makenaw <makena.walsh@gmail.com>
2016-01-27 21:58:18 +0800

    bringing in an parsing data

--- pokedex/pokemon.swift
+++ pokedex/pokemon.swift
@@ -44,7 +44,44 @@ class Pokemon {
         Alamofire.request(.GET, url).responseJSON{ response in
             let result = response.result
             
-            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+                
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+                
+                if let attack = dict["attack"] as? Int {
+                    self._attack = "\(attack)"
+                }
+                
+                if let defense = dict["defense"] as? Int {
+                    self._defense = "\(defense)"
+                }
+                print(self._height)
+                print(self._weight)
+                print(self._attack)
+                print(self._defense)
+                
+                if let types = dict["types"] as? [Dictionary<String, String>] where types.count > 0 {
+                    if let name = types[0]["name"] {
+                        self._type = name.capitalizedString
+                    }
+                    
+                    if types.count > 1 {
+                        for var x = 1; x < types.count; x++ {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name.capitalizedString)"
+                            }
+                        }
+                    }
+                } else {
+                    self._type = ""
+                }
+                print(self._type)
+            }
 
         }
     }
